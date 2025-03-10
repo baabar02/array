b2589f76124821cccd4e642b5ca6a07f6f411fb8
baabar02 <baabar02githu.com>
2025-03-10 12:34:47 +0800

    array

---
+++ array_method.js
@@ -0,0 +1,61 @@
+const animals = [];
+
+// array shine element nemeh- togsgold n
+animals.push("Bear");
+animals.push("Dog");
+animals.push("Mouse");
+animals.push("Horse");
+
+// array shine element nemeh- ehend n
+animals.push("Cat");
+animals.push("Fish");
+
+//["Fish", "Cat", "Bear", "Dog"]
+
+console.log("ani", animals);
+const ani = animals.pop();
+console.log("ani", animals);
+animals.shift();
+console.log("ani", animals);
+
+const res = animals.toString();
+console.log("res", res);
+const res2 = animals.join("-");
+console.log("res2", res2);
+const res3 = animals.indexOf("Fish");
+console.log("fish", res3);
+animals[res3] = "Sheep";
+console.log("res3", res3);
+console.log("res", animals);
+
+const res5 = animals.slice(0, 2);
+console.log(res5);
+console.log("ani", animals);
+
+const res6 = animals.splice(1, 1);
+console.log(res6);
+console.log("ani", animals);
+
+const res7 = animals.splice(2, 0, "Goat");
+console.log(res7);
+console.log("ani", animals);
+
+const input1 = [1, 2, 4];
+const input2 = input1.push("5");
+console.log(input1);
+
+const input = "the quick brown fox";
+const input3 = input.toLocaleUpperCase();
+console.log(input3);
+
+const arr1 = ["hello", "world", "javascript", "array"];
+const arr2 = arr1.reverse();
+console.log(arr2);
+
+const arr3 = [];
+for (const arr3 of arr1) {
+  const revArr = arr3.split("").reverse().join("");
+  arr3.push(revArr);
+}
+console.log(arr1);
+console.log(arr3);
---
+++ string.js
@@ -0,0 +1,96 @@
+console.log("string");
+
+const text = "Hello Pinecone. Welcome to world of JS";
+console.log(text[0]);
+
+for (let i = 0; i < text.length; i++) {
+  console.log(text[i]);
+}
+
+for (let char of text) {
+  console.log(char); // ugiig n ogno
+}
+
+for (let i in text) {
+  console.log(text[i]); // index iig n ogno
+}
+
+console.log(text.charAt(0));
+console.log(text.charCodeAt(0));
+console.log(text.endsWith(0));
+console.log(text.includes("Pinecone"));
+
+const str = " Hello String Pinecone";
+
+console.log(str.trim());
+console.log(str.replace("Hello", "Welcome"));
+
+const newStr = str.replace("Hello", "Welcome");
+console.log(newStr);
+
+const arr = str.trim().replace("Hello", "Welcome");
+
+console.log(str.split("String"));
+
+console.log(str.substring("hello"));
+
+const subStr = str.substring(5, 12);
+console.log(subStr);
+
+const i = str.indexOf("Pinecone");
+console.log(i);
+console.log(str.substring(i, i + 8));
+
+const str1 = " Welcome";
+const str2 = "Pinecone";
+const str3 = "Academy ";
+
+const myConcat = (...params) => {
+  console.log(params);
+  return str1 + str2 + str3;
+};
+
+const result = myConcat("hello", "pinecone", "Academy");
+console.log(result);
+
+const myConcat1 = (...params) => {
+  const res = " ";
+
+  for (let param of params) {
+    res = res + params[i];
+  }
+  return res;
+};
+
+// str1 = str1.toLowerCase("");
+console.log(str1);
+
+const isExistChar = (text, char) => {
+  for (let char of text) {
+    if (text[i] === char) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const indexOf = (text, char) => {
+  for (let char of text) {
+    if (text[i] === char) {
+      return i;
+    }
+  }
+  return -1;
+};
+const res = indexOf("Hello Pinecone", "K");
+console.log("Hello Pinecone");
+console.log(res);
+
+// console.log(char);
+
+// const myConcat = (str1, str2, str3) => {
+//     console.log(str1, str2, str3);
+//     return str1 + str2 + str3;
+//   };
+//   const result = myConcat("hello", "pinecone", "Academy");
+//   console.log(result);
